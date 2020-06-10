8b02af6bdd65b30f8fec97e94dd1e951b53dd5a6
andrew.reilly19 <andrew.reilly19@ncf.edu>
2020-06-10 03:19:32 -0400

    aggregate nearly finished (for now)

--- user_reports/step3_aggregate.py
+++ user_reports/step3_aggregate.py
@@ -9,15 +9,16 @@ Created on Fri May 22 09:45:30 2020
 import pandas as pd
 import numpy as np
 import datetime
+import os
 
 path = '/Users/andrew/Desktop/Riff_Analytics_Internship/analyze-data/user_reports/'
 
-df = pd.read_csv (path+'utterances_annotated.csv')
-df['startTime'] =  pd.to_datetime(df['startTime'])
-df['endTime'] =  pd.to_datetime(df['endTime'])
-meetings = df.meeting.unique()
-meetingsdf = pd.DataFrame(meetings)
-df = df.sort_values(by=['startTime'])
+dfinit = pd.read_csv (path+'utterances_annotated_almost.csv')
+dfinit['startTime'] =  pd.to_datetime(dfinit['startTime'])
+dfinit['endTime'] =  pd.to_datetime(dfinit['endTime'])
+df = dfinit.sort_values(by=['startTime'])
+
+print('loaded data')
 
 '''
 For aggregate meeting stats, filter overall df by utterances for one specific user
@@ -28,11 +29,13 @@ For aggregate meeting stats, filter overall df by utterances for one specific us
     - Brec Hanson : i6T3a2s5WpPo1dxZaRmIJlkFn4m1
     - Jordan : SDzkCh0CetQsNw2gUZS5HPX2FCe2
 '''
-#mike is in 4 meetings of the 5 test meetings
-select_participant = 'V4Kc1uN0pgP7oVhaDjcmp6swV2F3'
-#user_df.meeting.unique()
-dftest = df[df['meeting']=='music-2']
+known_users = {'jMZgnpJrX1QwAN0oUkYNme9kD4b2':'Amy','GcOHEObyGzTShEOpSzma6VpzT2Q2':'Beth','V4Kc1uN0pgP7oVhaDjcmp6swV2F3':'Mike','FJwf8UtoqvRJ4jnAaqzV5hcfxAG3':'John','i6T3a2s5WpPo1dxZaRmIJlkFn4m1':'Brec','SDzkCh0CetQsNw2gUZS5HPX2FCe2':'Jordan'}
 
+select_participant = 'V4Kc1uN0pgP7oVhaDjcmp6swV2F3' #Mike
+
+#only looking at meetings this participant was in
+meetings = dfinit[dfinit['participant']==select_participant].meeting.unique()
+meetingsdf = pd.DataFrame(meetings)
 
 meeting_length = []
 meeting_date = []
@@ -51,6 +54,8 @@ ap_dict = {key: 0 for key in all_participants}
 #Meeting Name, Date, length (mins)), users count, Target user speaking time (sec), count user interruptions, count user affirmations,
 #   count user influences, count user interruptees, count user affirmed, count user influenced
 
+counter = 0
+total_meet = len(meetings)
 for meeting in meetings:
     intermediateDF = df[df['meeting']==meeting]
     #meeting
@@ -65,9 +70,8 @@ for meeting in meetings:
     users = intermediateDF.participant.unique()
     n_users = len(users)
     meeting_user_count.append(n_users)
-    if select_participant in users:
-        for u in users:
-            ap_dict[u] += 1
+    for u in users:
+        ap_dict[u] += 1
 
     #participant information
     userDF = intermediateDF[intermediateDF['participant']==select_participant]
@@ -93,7 +97,10 @@ for meeting in meetings:
     #influences other user
     influences = intermediateDF[intermediateDF['influenced_by_user']==select_participant].shape[0]
     user_influences.append(influences)
+    counter += 1
+    print(counter, ' / ', total_meet)
 
+print('finalizing new dataframe...')
 
 meeting_length = pd.DataFrame(meeting_length)
 meeting_date = pd.DataFrame(meeting_date)
@@ -108,20 +115,118 @@ user_influences = pd.DataFrame(user_influences)
 
 meetingsdf = pd.concat([meetingsdf, meeting_length, meeting_date, meeting_user_count, user_speaking_time, user_interruptions], axis=1)
 meetingsdf = pd.concat([meetingsdf, user_affirmations, user_influenced, user_interrupted, user_affirmed, user_influences], axis=1)
-
 meetingsdf.columns = ['meeting_name','meeting_length_mins','meeting_date','total_participants','SU_speaking_time','SU_interruptions','SU_affirmations','SU_influenced_by','SU_interrupted','SU_affirmed','SU_influences']
 
-#writing out basic meetings data
-outpath = path + select_participant + '/'
-df.to_csv(outpath + 'all_meetings_aggregates.csv', index = None)
+
+#creating output path
+if select_participant in known_users:
+    outpath = path + known_users.get(select_participant) + '/'
+else:
+    outpath = path + select_participant + ''
+
+if not os.path.exists(outpath):
+    os.mkdir(outpath)
 
 
+#finding common users and outputting the 10 most common collaborators
 common_users = pd.DataFrame(list(ap_dict.items()), columns=['user','shared_meetings'])
 common_users = common_users[(common_users['user']!=select_participant) & (common_users['shared_meetings']>1)]
 most_common_buddy = common_users[(common_users['shared_meetings']==common_users.shared_meetings.max())].iloc[0]['user']
 
+top_collaborators = common_users.nlargest(10,['shared_meetings'])
+
+def add_names(row):
+    user_id = row['user']
+    try:
+        name = known_users.get(user_id)
+    except:
+        name = "na"
+    return(name)
+
+top_collaborators['names'] = top_collaborators.apply(lambda row: add_names(row), axis=1)
+top_collaborators.to_csv(outpath + 'other_known_users.csv', index = None)
 
 
+#now to get some comparisons for just meetings with the top collaborator (in Mike's data, this is Amy)
+
+with_best_buddy = {}
+without_best_buddy = {}
+
+for meeting in meetings:
+    intermediateDF = df[df['meeting']==meeting]
+    meeting_users = intermediateDF.participant.unique()
+    if most_common_buddy in meeting_users:
+        with_best_buddy[meeting]=1
+    else:
+        without_best_buddy[meeting]=0
+
+with_best_buddy = pd.DataFrame(list(with_best_buddy.items()), columns=['meeting1','with_top_collaborator'])
+without_best_buddy = pd.DataFrame(list(without_best_buddy.items()), columns=['meeting1','with_top_collaborator'])
+
+bb = pd.concat([with_best_buddy, without_best_buddy])
+meetingsdf = meetingsdf.merge(bb, left_on="meeting_name", right_on="meeting1")
+meetingsdf = meetingsdf.drop('meeting1', axis=1)
+
+df.to_csv(outpath + 'all_meetings_aggregates.csv', index = None)
+
+
+#finally, creating one last document strictly meant for graphing purposes - these will be combined on
+#a weekly basis and it will be utterances/min, etc. per week
+
+date_meetingsdf = meetingsdf.drop('meeting_name', axis=1)
+date_meetingsdf['meeting_date'] = pd.to_datetime(date_meetingsdf['meeting_date'])
+
+def getweek(row):
+    week = row['meeting_date'].strftime("%V")
+    return(week)
+
+def getmonth(row):
+    month = row['meeting_date'].strftime("%m")
+    return(month)
+
+def getyear(row):
+    year = row['meeting_date'].strftime("%Y")
+    return(year)
+
+def group_nusers(row):
+    nusers = row['total_participants']
+    if nusers == 2:
+        return("2")
+    if nusers>2 & nusers<=4:
+        return("3-4")
+    if nusers>4 & nusers<=7:
+        return("5-7")
+    if nusers>7:
+        return("8+")
+
+date_meetingsdf['meeting_week'] = date_meetingsdf.apply(lambda row: getweek(row), axis=1)
+date_meetingsdf['meeting_month'] = date_meetingsdf.apply(lambda row: getmonth(row), axis=1)
+date_meetingsdf['meeting_year'] = date_meetingsdf.apply(lambda row: getyear(row), axis=1)
+date_meetingsdf['total_participants'] = date_meetingsdf.apply(lambda row: group_nusers(row), axis=1)
+
+week_meetingsdf = date_meetingsdf.drop(['meeting_date', 'meeting_month'],axis=1)
+month_meetingsdf = date_meetingsdf.drop(['meeting_date', 'meeting_week'],axis=1)
+
+week_meetingsdf_noUs = week_meetingsdf.drop(['total_participants','with_top_collaborator'],axis=1)
+month_meetingsdf_noUs = month_meetingsdf.drop(['total_participants','with_top_collaborator'],axis=1)
+
+week_meetingsdf_noTP = week_meetingsdf.drop(['total_participants'],axis=1)
+month_meetingsdf_noTP = month_meetingsdf.drop(['total_participants'],axis=1)
+
+week_meetingsdf_noTC = week_meetingsdf.drop(['with_top_collaborator'],axis=1)
+month_meetingsdf_noTC = month_meetingsdf.drop(['with_top_collaborator'],axis=1)
+
+
+week_meetingsdf_noUs = week_meetingsdf_noUs.groupby(['meeting_year','meeting_week']).sum()
+month_meetingsdf_noUs = month_meetingsdf_noUs.groupby(['meeting_year','meeting_month']).sum()
+
+week_meetingsdf_noTP = week_meetingsdf_noUs.groupby(['meeting_year','meeting_week','with_top_collaborator']).sum()
+month_meetingsdf_noTP = month_meetingsdf_noUs.groupby(['meeting_year','meeting_month','with_top_collaborator']).sum()
+
+week_meetingsdf_noTC = week_meetingsdf_noUs.groupby(['meeting_year','meeting_week','total_participants']).sum()
+month_meetingsdf_noTC = month_meetingsdf_noUs.groupby(['meeting_year','meeting_month', 'total_participants']).sum()
+
+print('done!')
 
 
 
